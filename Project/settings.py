1c2a947672fbdc9f17769b62d77dc236d2240f5c
AnasAttoum <anasattoum.12321@gmail.com>
2023-07-27 14:20:52 +0300

    Edits

--- Project/settings.py
+++ Project/settings.py
@@ -271,7 +271,7 @@ JAZZMIN_SETTINGS = {
     #############
     # Relative paths to custom CSS/JS scripts (must be present in static files)
     # Uncomment this line once you create the bootstrap-dark.css file
-    "custom_css": "Bootstrap.css",
+    "custom_css": "AdminCSS.css",
     
 
     # "custom_css": "bootstrap-dark.css",
